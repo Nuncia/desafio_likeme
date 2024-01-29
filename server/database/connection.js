dace59c4648dc897bf763f22d2777e56a3623e04
Monica <maaylahuerta@gmail.com>
2024-01-29 20:24:38 -0300

    Se modifican variables de entorno

--- server/database/connection.js
+++ server/database/connection.js
@@ -8,7 +8,7 @@ export const pool = new Pool({
    user: process.env.DB_USER,
    host: process.env.DB_HOST,
    database: process.env.DB_DATABASE,
-   password: '',
+   password: process.env.DB_PASSWORD,
    port: 5433,
    allowExitOnIdle: true,
 });
